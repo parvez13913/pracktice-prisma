c19deb70d8270b140649be13fb674823fe772345
parvez13913 <parvezz13913@gmail.com>
2023-09-08 15:37:08 +0600

    [DONE] Prisma Client create and read

---
+++ index.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@prisma/client";
+
+const pisma = new PrismaClient();
+
+async function main() {
+  const getAllUsers = await pisma.user.findMany();
+  console.log(getAllUsers);
+  //   const postUser = await pisma.user.create({
+  //     data: {
+  //       name: "Abir Hassan",
+  //       email: "abir14016@gmail.com",
+  //     },
+  //   });
+
+  //   console.log(postUser);
+}
+
+main();
